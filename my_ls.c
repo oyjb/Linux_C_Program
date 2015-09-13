c092d28ab39acc04ce85be7a07f240ec7b5c1118
oyjb <jbouyang@126.com>
2015-09-13 22:38:24 +0800

    add my_ls.c

---
+++ my_ls.c
@@ -0,0 +1,16 @@
+/**********************************************************
+ * Author        : oyjb
+ * Email         : jbouyang@126.com
+ * Last modified : 2015-09-13 22:37
+ * Filename      : my_ls.c
+ * Description   : copy right for oyjb
+ * *******************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <errno.h>
+#include <string.h>
+
+#define MAX 1024
+
+void 
